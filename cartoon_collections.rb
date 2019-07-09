2c7c5c582cda328222747888d3140eded9de52ff
LaNice Powell <lanicemp@aol.com>
2019-07-09 17:51:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,9 @@ def long_planeteer_calls(array)
   array.any? { |call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |type|
+    cheese_types.include?(type)
 end
+end 
